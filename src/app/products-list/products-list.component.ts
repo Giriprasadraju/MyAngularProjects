e86d7bedd36b693cefb08efc311bc18495f171e0
Giriprasadraju <vngpraju2001@gmail.com>
2023-01-14 14:32:16 +0530

    HttpServiceImplementation

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,41 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { IProduct } from './Product';
+import { ProductService } from './products.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent implements OnInit{
+export class ProductsListComponent implements OnInit, OnDestroy{
 
   pageTitle: string ="ProdManagement";
   showImage : boolean =false;
   imageWidth : number =50;
   imageMargin : number =10;
+  errorMessage : string ="";
+  sub!: Subscription;
   filteredProducts: IProduct[] = [];
 
-  products: any[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 3,
-      "imageUrl": "./assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.5,
-      "imageUrl": "./assets/images/hammer.png"
-    }
-  ];
+  products: IProduct[] = [];
 
   showImageEvent(): void
 {
@@ -52,6 +35,10 @@ this._filterText=value;
 this.filteredProducts = this.performFilter(value);
 }
 
+constructor(private productService : ProductService) {
+
+}
+
 
 performFilter(filterBy: string): IProduct[] {
   filterBy = filterBy.toLocaleLowerCase();
@@ -60,8 +47,20 @@ performFilter(filterBy: string): IProduct[] {
 }
 
 ngOnInit(): void {
-  this.filterText= "";
+  this.sub = this.productService.getProducts().subscribe({
+    next: products => {
+      this.products = products;
+      this.filteredProducts = this.products
+    },
+    error: err => this.errorMessage =err
+
+});
+this.filteredProducts = this.products;
 }
+ngOnDestroy(): void {
+  this.sub.unsubscribe();
+}
+
 onRatingClicked(message : string): void {
   this.pageTitle = 'ProductList:  ' + message;
 }
